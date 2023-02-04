e75e08be6684b9b06f6930e070a830224edb4296
tofsona14 <topuria2074@gmail.com>
2023-02-04 18:22:23 +0400

    add second page styles and jsx code

--- src/PageSources/SecondPage/SecondComponents/SecondSecond.js
+++ src/PageSources/SecondPage/SecondComponents/SecondSecond.js
@@ -8,6 +8,29 @@ const SecondSecond = () => {
                 <h2>პირადი ინფო</h2>
                 <h3 style={{ color: "gray"}}>1/3</h3>
             </div>
+            <div className='Second--Body'>
+                <div>
+                    <div className='Second--Body--First--First'>
+                        <div>
+                            <label>სახელი</label>
+                            <div>
+                            <input className='Second--Body--First--First--Input' placeholder='ანზორ' minLength={2}></input>
+                            <p className='Second--Body--First--First-P' style={{color:"gray"}}>მინიმუმ 2 ასო, ქართული ასოებით</p>
+                            </div>
+                        </div>
+                        <div>
+                            <label>გვარი</label>
+                            <div>
+                            <input className='Second--Body--First--First--Input' placeholder='მუმლაძე' minLength={2}></input>
+                            <p className='Second--Body--First--First-P' style={{color:"gray"}}>მინიმუმ 2 ასო, ქართული ასოებით</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='Second--Body--First--Second'>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
