cb374ef787c51ce2d9bc190ae7ffa6db9139f604
tofsona14 <topuria2074@gmail.com>
2023-02-04 13:33:02 +0400

    add background and styles

---
+++ src/PageSources/MainPage/MainComponents/mainHeader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './mainHeader.css'
+
+
+const MainHeader = () => {
+    return(
+    <div className='Header--Main'>
+        <h1>REDBERRY</h1>
+    </div>
+    )
+}
+
+export default MainHeader
