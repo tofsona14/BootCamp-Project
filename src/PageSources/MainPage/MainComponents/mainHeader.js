61c03394cc191df86bb049abbf602a05e91b7126
tofsona14 <topuria2074@gmail.com>
2023-02-04 18:27:05 +0400

    add arrow to back to main page

--- src/PageSources/MainPage/MainComponents/mainHeader.js
+++ src/PageSources/MainPage/MainComponents/mainHeader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './MainHeader.css'
+import { Link } from 'react-router-dom'
 
 
 const MainHeader = () => {
--- src/PageSources/SecondPage/SecondComponents/SecondFirst.js
+++ src/PageSources/SecondPage/SecondComponents/SecondFirst.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './SecondFirst.css'
 import image from '../../images/s.png'
+import { Link } from 'react-router-dom'
 
 
 const SecondFirst = () => {
     return(
         <div className='SecondHeader--Main'>
-            <div className='left--arrows'><img className='left--arrow' src={image}></img></div>
+            <Link to="/"><div className='left--arrows'><img className='left--arrow' src={image}></img></div></Link>
             
         </div>
     )
