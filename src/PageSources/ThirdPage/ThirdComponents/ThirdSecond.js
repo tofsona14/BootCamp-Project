0d0f2f9389364f486b03c4ac45b1d0c5b67f3a89
tofsona14 <topuria2074@gmail.com>
2023-02-13 11:41:06 +0400

    fix validations

--- src/PageSources/ThirdPage/ThirdComponents/ThirdSecond.js
+++ src/PageSources/ThirdPage/ThirdComponents/ThirdSecond.js
@@ -10,29 +10,24 @@ const ThirdSecond = () => {
     const formValidation = (se) => {
         const s = [...se]
         for(let index = 0; index < s.length; index++) {
-            if(s[index].position == "") {
-                s[index].positionCheck = "position required"
-            }else if (s[index].position[0].length < 2){
-                s[index].positionLengthCheck = "position need minimum 2 symbol"
+            if(s[index].position == "" || s[index].position[0].length < 2) {
+                s[index].positionErr = "required"
+            
             }else {
 
-                s[index].positionCheck = ""
-                s[index].positionLengthCheck = ""
+                s[index].positionErr = ""
             }
-            if(s[index].employer == "") {
-                s[index].employerCheck = "employer required"
-            }else if (s[index].employer[0].length < 2){
-                s[index].employerLengthCheck = "employer need minimum 2 symbol"
+            if(s[index].employer == "" || s[index].employer[0].length < 2) {
+                s[index].employerErr = "required"
+            
             }else {
 
-                s[index].employerCheck = ""
-                s[index].employerLengthCheck = ""
+                s[index].employerErr = ""
             }
             setData(s)
         }
     }
-    
-    const [data, setData] = useState([{position: "",employer: "",startTime: "",endTime: "",otherD: ""}])
+    const [data, setData] = useState([{position: "",employer: "",startTime: "",endTime: "",otherD: "",positionErr:"",employerErr:""}])
     const [sama, setSama] = useState([{
         first: "გამოცდილება",
         second: "თანამდებობა",
@@ -44,7 +39,7 @@ const ThirdSecond = () => {
         eight: "მინიმუმ 2 სიმბოლო"
     }])
     const addRow = () => {
-        setData([...data, {position: "",employer: "",startTime: "",endTime: "",otherD: ""}]) 
+        setData([...data, {position: "",employer: "",startTime: "",endTime: "",otherD: "",positionErr:"", employerErr:""}]) 
         setSama((prev) => {
             return [...prev, {
             first: "გამოცდილება",
@@ -60,12 +55,42 @@ const ThirdSecond = () => {
     
     )   
     } 
+    const [boolean, setBoolean] = useState("")
+    const [required,setRequired] = useState([])
     const handleChange = (e, i) => {
         let newForm = [...data]
         newForm[i][e.target.name] = [e.target.value]
         setData(newForm)
         formValidation(data)
         console.log(data)
+        setRequired((prev) => {
+            let s = [...prev]
+            if(data[0].position == "" || data[0].employer == "" || data[0].startTime == "" || data[0].endTime == "" || data[0].otherD == "" || data[0].positionErr != "" || data[0].employerErr != ""  ) {
+               s[0] = 'required'
+               setRequired(s) 
+            }else {
+                s[0] = ""
+                setRequired(s)
+            }
+            if(i != 0 && data[i].position == "" && data[i].employer == "" && data[i].startTime == "" && data[i].endTime == "" && data[i].otherD == "" ) {
+                s[i] = ""
+                setRequired(s)
+            }if(i != 0 && data[i].employerErr == "" && data[i].positionErr == "" ) {
+                s[i] = ''
+                setRequired(s)
+            }
+            else {
+                if(i != 0) {
+                    s[i] = 'required'
+                    setRequired(s)
+                }
+            
+        }
+            }
+
+        )
+        setBoolean(required.includes('required'))
+        
     }
     useEffect(() => {
         const secondVal = window.localStorage.getItem("val-2")
@@ -73,6 +98,35 @@ const ThirdSecond = () => {
             setData(JSON.parse(secondVal))
         }
 },[])
+    data.map((e, i) => {
+        setRequired((prev) => {
+            let s = [...prev]
+            if(data[0].position == "" || data[0].employer == "" || data[0].startTime == "" || data[0].endTime == "" || data[0].otherD == "" || data[0].positionErr != "" || data[0].employerErr != ""  ) {
+               s[0] = 'required'
+               setRequired(s) 
+            }else {
+                s[0] = ""
+                setRequired(s)
+            }
+            if(i != 0 && data[i].position == "" && data[i].employer == "" && data[i].startTime == "" && data[i].endTime == "" && data[i].otherD == "" ) {
+                s[i] = ""
+                setRequired(s)
+            }if(i != 0 && data[i].employerErr == "" && data[i].positionErr == "" ) {
+                s[i] = ''
+                setRequired(s)
+            }
+            else {
+                if(i != 0) {
+                    s[i] = 'required'
+                    setRequired(s)
+                }
+            
+        }
+            }
+
+        )
+        setBoolean(required.includes('required'))
+    })
    
     useEffect(() => {
         setTimeout(() => {
@@ -80,7 +134,8 @@ const ThirdSecond = () => {
             window.localStorage.setItem("val-2", JSON.stringify(data))
         }, 400)
     })
-    
+    console.log(boolean)
+    console.log(`${required}......`)
 return(
         <div className='Third--Second'>
             <div className='Third--Header'>
@@ -94,14 +149,14 @@ return(
                     <label htmlFor='position'>{e.second}</label>
                     <br></br>
                     <div >
-                        <input    id="position" name='position' onChange={(e) => handleChange(e,i)} value={data[i].position || ""} className={data[i].position.length == 0 ? 'Third--Body--First--Input': data[i].positionCheck || data[i].positionLengthCheck ? "Third--Body--First--Input--declined" : "Third--Body--First--Input--accepted"} placeholder='დეველოპერი, დიზაინერი და ა.შ'></input>
+                        <input    id="position" name='position' onChange={(e) => handleChange(e,i)} value={data[i].position || ""} className={data[i].position.length == 0 ? 'Third--Body--First--Input':  data[i].positionErr != "" ? "Third--Body--First--Input--declined" : "Third--Body--First--Input--accepted"} placeholder='დეველოპერი, დიზაინერი და ა.შ'></input>
                         <p className="Third--Body--First--First-P" style={{color:"#2E2E2E"}} >{e.eight}</p>
                     </div>
                 </div>
                 <div className='Third--Second--Body--Second'>
                     <label htmlFor='employer'>{e.third}</label>
                     <div >
-                        <input  id="employer" name='employer' onChange={(e) => handleChange(e,i)} value={data[i].employer || ""} className={data[i].employer.length == 0 ?'Third--Body--First--Input': data[i].employerCheck || data[i].employerLengthCheck ? "Third--Body--First--Input--declined" : "Third--Body--First--Input--accepted" } placeholder='დამსაქმებელი'></input>
+                        <input  id="employer" name='employer' onChange={(e) => handleChange(e,i)} value={data[i].employer || ""} className={data[i].employer.length == 0 ?'Third--Body--First--Input': data[i].employerErr != "" ? "Third--Body--First--Input--declined" : "Third--Body--First--Input--accepted" } placeholder='დამსაქმებელი'></input>
                         <p className="Third--Body--First--First-P" style={{color:"#2E2E2E"}} >{e.eight}</p>
                     </div>
                 </div>
@@ -130,8 +185,9 @@ return(
             </div>
             )) }
                 <div className='Third--Body--Fifth'>
+                    
                     <Link to="/Private-Info"><button className='Third--Body--Fifth--st--button'>უკან</button></Link>
-                    <Link to="/Education"><button className='Third--Body--Fifth--nd--button'>შემდეგი</button></Link>
+                    {boolean == false ? <Link to="/Education"><button className='Third--Body--Fifth--nd--button'>შემდეგი</button></Link>:<button className='Third--Body--Fifth--nd--button'>შემდეგი</button>}
                 </div>
         </div>
     )
